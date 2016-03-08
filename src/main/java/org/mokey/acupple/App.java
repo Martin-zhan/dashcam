d16fcf9764fee39d5714ae3d64475a5bcc239123
enousei <shenhuarj@gmail.com>
2016-03-08 16:40:01 +0800

    init project

---
+++ src/main/java/org/mokey/acupple/App.java
@@ -0,0 +1,13 @@
+package org.mokey.acupple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
